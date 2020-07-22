dbcf2ae12c453b92c56b17c993237940b8699fe7
Leah Russell <leah@procore.com>
2020-07-21 21:59:53 -0700

    Changing config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module GiveNTake
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.autoload_paths << "#{config.root}/lib/validators"
+		Rails.autoloaders.main.ignore("#{Rails.root}/app/overrides")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
